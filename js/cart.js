a87b4261074c42e7888a5c1afb2ac3f842650912
Alex <alexil90@yandex.ru>
2021-06-09 16:16:28 +0200

    Задание 1
    - заготовка для классов корзины

---
+++ js/cart.js
@@ -0,0 +1,30 @@
+//задание 1
+class CartItem {
+    constructor() {
+      
+    }
+    changeQuantity() {
+
+    }
+    render() {
+
+    }
+}
+
+class Cart {
+    constructor() {
+      
+    }
+    addCartItem() {
+
+    }
+    removeCartItem() {
+
+    }
+    countTotalPrice() {
+
+    }
+    render() {
+
+    }
+}
